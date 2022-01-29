9374c1ba57846016dd563896ee315c587969379c
elliot <xianman125@163.com>
2022-01-29 18:22:04 +0800

    ch16_sec2_type_transformation

--- ch16/blob.h
+++ ch16/blob.h
@@ -4,18 +4,22 @@
 #include <vector>
 // 前置声明; 在Blob中声明友元所需要的
 // 声明模板；模板声明包含模板参数列表
-template <typename> class BlobPtr;
-template <typename> class Blob;
+template <typename>
+class BlobPtr;
+template <typename>
+class Blob;
 
-template <typename T> bool operator==(const Blob<T> &, const Blob<T> &);
+template <typename T>
+bool operator==(const Blob<T> &, const Blob<T> &);
 
-template <typename T> class Blob {
+template <typename T>
+class Blob {
   friend class BlobPtr<T>;
   // 友元的声明使用Blob的模板形参作为自己模板实参
   // 因此友好关系被限定在用相同类型实例化的Blob与BlobPtr相等运算符之间。
   friend bool operator==<T>(const Blob<T> &, const Blob<T> &);
 
-public:
+ public:
   typedef T value_type;
   typedef typename std::vector<T>::size_type size_type;
 
@@ -36,45 +40,51 @@ public:
   T &back();
   T &operator[](size_type i);
 
-private:
+ private:
   std::shared_ptr<std::vector<T>> data;
   void check(size_type i, const std::string &msg) const;
 };
 
 template <typename T>
 void Blob<T>::check(size_type i, const std::string &msg) const {
-  if (i >= data->size())
-    throw std::out_of_range(msg);
+  if (i >= data->size()) throw std::out_of_range(msg);
 }
 
-template <typename T> T &Blob<T>::back() {
+template <typename T>
+T &Blob<T>::back() {
   check(0, "back on empty blob");
   return data->back();
 }
 
-template <typename T> void Blob<T>::pop_back() {
+template <typename T>
+void Blob<T>::pop_back() {
   check(0, "pop_back on empty blob");
   data->pop_back();
 }
 
-template <typename T> T &Blob<T>::operator[](size_type i) {
+template <typename T>
+T &Blob<T>::operator[](size_type i) {
   check(i, "subscript out of range");
   return (*data)[i];
 }
 
 // 一个类可以将另一个模板的每个实例都声明为自己的友元。或者限定特定的实例为友元。
 // 前置声明，在将模板的一个特定实例声明为友元要用到。
-template <typename T> class Pal;
+template <typename T>
+class Pal;
 class C {
-  friend class Pal<C>; // 用类C实例化的Pal是C的一个友元
+  friend class Pal<C>;  // 用类C实例化的Pal是C的一个友元
   // Pal2的所有实例都是C的友元; 此情况无须前置声明
-  template <typename T> friend class Pal2;
+  template <typename T>
+  friend class Pal2;
 };
 
-template <typename T> class C2 {
+template <typename T>
+class C2 {
   // C2的每个实例将相同实例化的Pal声明为友元
   friend class Pal<T>;  // Pal的模板声明必须在作用域之内;
-  template <typename X> friend class Pal2; // Pal2的所有实例都是C2的每个实例的友元,不需要前置声明
+  template <typename X>
+  friend class Pal2;  // Pal2的所有实例都是C2的每个实例的友元,不需要前置声明
   // Pal3 是一个非模板类，它是C2所有实例的友元。
-  friend class Pal3; //不需要Pal3的前置声明。
-}
+  friend class Pal3;  //不需要Pal3的前置声明。
+};
--- ch16/main.cpp
+++ ch16/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
 #include "blob.h"
+#include "sec_02_type_trans.h"
 
 using std::cout;
 using std::endl;
-int main() {
+
+void test_sec_02_type_trans() {
+  std::string str = "hello";
+  char s = sec_02::fcn2(str.begin(), str.end());
+  cout << s << endl;
+}
+
+void test_blob() {
   Blob<int> blob;
   for (size_t i = 0; i < 100; i++) {
     blob.push_back(i);
@@ -15,3 +23,4 @@ int main() {
   cout << "size: " << blob.size() << endl;
   cout << "empty: " << blob.empty() << endl;
 }
+int main() { test_sec_02_type_trans(); }
